56434c974afe226638a76cc0966156582e50b889
Ravi <ravi.bhusal.np@gmail.com>
2019-08-21 10:25:06 +0545

    Initial Commit

---
+++ src/components/board.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Board extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div class="game-board">
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+        <div class="box">A</div>
+      </div>
+    );
+  }
+}
+
+export default Board;
